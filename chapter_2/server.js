a3598d8b02e62146ba877c17772e82fbcec22e3d
Limbbuu <linus.nurminen@gmail.com>
2025-05-22 11:26:56 +0300

    install --save-dev nodemon to run server without a need to restart it everytime before running script

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -2,4 +2,4 @@ const express = require('express'); // requires Express and store it in to varia
 const app = express(); //define backend application and invoke Express as a Function
 const PORT = 8383; //define port, 4-digit number typically
 
-app.listen(PORT, () => console.log(`Server has started on ${PORT}`)); //listens the incoming request
+app.listen(PORT, () => console.log(`Server has started on: ${PORT}`)); //listens the incoming request
