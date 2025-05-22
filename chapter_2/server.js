2862b146790ad59fa959500b9da91cf1e1223a80
Limbbuu <linus.nurminen@gmail.com>
2025-05-22 11:15:34 +0300

    create server that listens incoming requests over the interned

---
+++ chapter_2/server.js
@@ -0,0 +1,5 @@
+const express = require('express'); // requires Express and store it in to variable
+const app = express(); //define backend application and invoke Express as a Function
+const PORT = 8383; //define port, 4-digit number typically
+
+app.listen(PORT, () => console.log(`Server has started on ${PORT}`)); //listens the incoming request
