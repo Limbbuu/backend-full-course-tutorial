4967b49b31edc6dfd6687ead31c8b5b5d9fc3262
Limbbuu <linus.nurminen@gmail.com>
2025-06-03 18:17:04 +0300

    encrypt the password and added comments to explain the encyption process

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -9,11 +9,20 @@ const router = express.Router();
 // Register a new user endpoint /auth/register
 router.post('/register', (req, res) => {
     const {username, password} = req.body;
-    console.log(username, password);
+    // save username and an irreversibly encrypted to database
+
+    // encrypt the password
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    
+    console.log(hashedPassword);
+
     res.sendStatus(201);
 });
 
 router.post('/login', (req, res) => {
+    // we get their email, and we look up the password associated with that email in the database
+    // but we get it back and see it's encrypted, which means that we cannot compare it to the one the user just used trying to login
+    // so what we can to do, is again, one way encrypted the password the user just entered
 
 });
 
